05aa6fedc88583579ca656fc2c419f29d29f6f07
ZacharyLeicht2 <75800317+ZacharyLeicht2@users.noreply.github.com>
2021-04-19 12:27:40 -0400

    Added Key Sounds, and Added Option of choosing what plays what!

--- Key.java
+++ Key.java
@@ -2,11 +2,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 
 public class Key extends Actor
 {
+    private boolean isDown;
+    private String key;
+    private String sound; 
+    
     /**
-     * Create a new key.
+     * Create a new key linked to a given keyboard key, and
+     * with a given sound.
      */
-    public Key()
+    public Key(String keyName, String soundFile)
     {
+        key = keyName;
+        sound = soundFile;
     }
 
     /**
@@ -14,5 +21,25 @@ public class Key extends Actor
      */
     public void act()
     {
+        if(!isDown && Greenfoot.isKeyDown(key))
+        {
+           setImage("white-key-down.png");
+           Greenfoot.playSound("sound");
+           isDown = true;
+        }
+        if( isDown && !Greenfoot.isKeyDown(key))
+        {
+           setImage("white-key.png"); 
+           isDown = false; 
+        }
+    }
+    
+    /*
+     * Play The Note of this Key  
+     */
+    public void play()
+    {
+        Greenfoot.playSound("sound"); 
     }
 }
+    
